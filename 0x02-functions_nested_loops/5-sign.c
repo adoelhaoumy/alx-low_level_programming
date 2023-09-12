fe99f9e2f488fed6ad443beaa585909eedd5a082
abdelhahm <alhoumi20@gmail.com.com>
2023-09-12 03:32:47 -0700

    5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 
 /**
@@ -11,17 +12,15 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-        _putchar(43);
-        return (1);
-	} 
-        else if (n <1);
-        }
-	_putchar(45);
-	return (-1);
-	}
-         else 
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+}
+		_putchar(48);
+		return (0);
+	} else if (n < 0)
 	{
-        _putchar(48);
-        return (0);
+		_putchar('-');
 	}
+		return (-1);
 }
